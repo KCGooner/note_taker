f2ca7497532ef4beaf2e5506351223c1c7e77b1c
Joe Huelsman <kcgooner91@gmail.com>
2020-02-25 18:46:02 -0600

    added server and routes to site

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express")
+var path = require("path")
+
+//sets up express app
+var app = express();
+var PORT = 3000;
+
+//sets up express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+
+//start server to listen
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
