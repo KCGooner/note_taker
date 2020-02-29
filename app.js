58b018252645b1c8d4c145937fc1dcee7443ba77
Joe Huelsman <kcgooner91@gmail.com>
2020-02-29 13:01:38 -0600

    added delete functionality for notes

--- app.js
+++ app.js
@@ -21,25 +21,39 @@ app.get("/notes", function (req, res) {
 });
 
 app.get("/api/notes", (req, res) => {
-
     res.sendFile(path.join(__dirname, "/db.json"));
 });
 
 app.post("/api/notes", (req, res) => {
-    
-    const data = req.body;
+
+    const dataObj = req.body;
     const id = Date.now();
-    const db = fs.readFileSync(path.join(__dirname, "/db.json"), "utf8");
-    
+    const db = fs.readFileSync(path.join(__dirname, "/db.json"));
+
     const note = {
         id: id,
-        title: data.title,
-        text: data.text,
+        title: dataObj.title,
+        text: dataObj.text,
     };
     const dbfile = JSON.parse(db)
     dbfile.push(note);
-    fs.writeFileSync(path.join(__dirname, "/db.json"), JSON.stringify(dbfile), "utf8");
+    fs.writeFileSync(path.join(__dirname, "/db.json"), JSON.stringify(dbfile));
     res.sendFile(path.join(__dirname, "/db.json"));
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+
+    const id = req.params.id;
+    const db = fs.readFileSync(path.join(__dirname, "/db.json"));
+    const dbFile = JSON.parse(db);
+    for (let x = 0; x < dbFile.length; x++) {
+        if (dbFile[x].id.toString() === id) {
+            dbFile.splice(x, 1);
+            break;
+        }
+    }
+
+
 });
 
 //start server to listen
